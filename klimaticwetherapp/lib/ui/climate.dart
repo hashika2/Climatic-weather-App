a4bdc4e57781ae880d171beb0c3d902d782b05df
hashika <m.g.hashikamaduranga@gmail.com>
2019-09-15 20:22:02 +0530

    set background image

--- klimaticwetherapp/lib/ui/climate.dart
+++ klimaticwetherapp/lib/ui/climate.dart
@@ -15,16 +15,45 @@ class Klimate extends StatefulWidget{
    
     return Scaffold(
       appBar: new AppBar(
-        title: new Text("Climate",
-        style: new TextStyle(color: Colors.black),),
-        backgroundColor: Colors.white,
+        title: new Text("Klimate",
+        style: new TextStyle(color: Colors.white),),
+        backgroundColor: Colors.blueAccent,
         centerTitle: true,
         actions: <Widget>[
-          
+          new IconButton(
+            icon: new Icon(Icons.menu),
+            onPressed:() =>debugPrint("hashika"),
+          )
           
           
         ],
       ),
-    );
-  }
-}
+      body: new Stack(
+        children: <Widget>[
+          new Center(
+            child: new Image.asset('images/umbrella.png',
+            width: 400.0,
+            height: 1100.0,
+            fit: BoxFit.fill,// full size of the screen
+            ),
+          ),
+          new Container(
+            alignment:Alignment.topRight,
+            padding:const EdgeInsets.fromLTRB(0.0, 10.3, 10.3, 0.0),
+            child: new Text("spaonerd",
+            style: CityStyle()),
+                      )
+                    ],
+                  ),
+                );
+              }
+            }
+            
+             CityStyle() {  // we can style as using methods for fonts etc
+              return  new TextStyle(
+                color: Colors.white,
+                fontSize: 30.5,
+                fontStyle: FontStyle.italic
+
+              );
+             }
